375bc6ecf2c7f7b7f156ce01dd77a68b22fcb9e7
WN <cluodyt@gmail.com>
2024-03-14 03:15:34 +0300

    Delete TeamCode/robotcontroller directory

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/minirobot/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/minirobot/test.java
@@ -6,28 +6,24 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name="minirobot", group="Iterative Opmode")
+@TeleOp(name="test", group="Iterative Opmode")
 
-public class minirobot extends OpMode
+public class test extends OpMode
 {
-    // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor right = null;
-    private DcMotor back_right = null;
-    private DcMotor left = null;
-    private DcMotor back_left = null;
     private DcMotor arm = null;
     private Servo armLeft = null;
     private Servo armRight = null;
     private Servo wrist = null;
+    private ElapsedTime runtime = new ElapsedTime();
+
 
     private boolean manualMode = false;
     private double armSetpoint = 0.0;
 
     private final double armManualDeadband = 0.03;
 
-    private final double gripperClosedPosition = 1.0;
-    private final double gripperOpenPosition = 0.5;
+    //private final double gripperClosedPosition = -1;
+   // private final double gripperOpenPosition = 1;
     private final double wristUpPosition = 0.5;
     private final double wristDownPosition = -0.3;
 
@@ -36,26 +32,14 @@ public class minirobot extends OpMode
     private final int armScorePosition = 600;
     private final int armShutdownThreshold = 5;
 
-
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
     @Override
     public void init() {
         telemetry.addData("Status", "Initialized");
 
-        right = hardwareMap.get(DcMotor.class, "right");
-        back_right = hardwareMap.get(DcMotor.class, "back_right");
-        left = hardwareMap.get(DcMotor.class, "left");
-        back_left = hardwareMap.get(DcMotor.class, "back_left");
-        arm  = hardwareMap.get(DcMotor.class, "arm");
+        arm = hardwareMap.get(DcMotor.class, "arm");
         armLeft = hardwareMap.get(Servo.class, "armLeft");
         armRight = hardwareMap.get(Servo.class, "armRight");
         wrist = hardwareMap.get(Servo.class, "wrist");
-
-        right.setDirection(DcMotor.Direction.REVERSE);
-        back_right.setDirection(DcMotor.Direction.REVERSE);
-
 
         arm.setDirection(DcMotor.Direction.FORWARD);
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -65,49 +49,26 @@ public class minirobot extends OpMode
 
         telemetry.addData("Status", "Initialized");
     }
+        @Override
+        public void init_loop() {
+        }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
-    @Override
-    public void init_loop() {
-    }
+        /*
+         * Code to run ONCE when the driver hits PLAY
+         */
+        @Override
+        public void start() {
+            runtime.reset();
 
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
-    @Override
-    public void start() {
-        runtime.reset();
+            arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            arm.setTargetPosition(armHomePosition);
+            arm.setPower(0.5);
+            arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
 
-        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        arm.setTargetPosition(armHomePosition);
-        arm.setPower(0.5);
-        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-    }
-
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
     @Override
     public void loop() {
         double manualArmPower;
-
-        //DRIVE
-        double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-        double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-        double rx = gamepad1.right_stick_x;
-
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-        double frontLeftPower = (y + x + rx) / denominator;
-        double backLeftPower = (y - x + rx) / denominator;
-        double frontRightPower = (y - x - rx) / denominator;
-        double backRightPower = (y + x - rx) / denominator;
-
-        right.setPower(frontLeftPower);
-        left.setPower(backLeftPower);
-        back_right.setPower(frontRightPower);
-        back_left.setPower(backRightPower);
 
         //ARM & WRIST
         manualArmPower = gamepad1.left_trigger - gamepad1.right_trigger;
@@ -162,24 +123,31 @@ public class minirobot extends OpMode
 
         //GRIPPER
         if (gamepad1.left_bumper) {
-            armLeft.setPosition(gripperOpenPosition);
+            armLeft.setPosition(-1);
         }
 
-        else {
-            armLeft.setPosition(gripperClosedPosition);
+        if (gamepad1.dpad_left) {
+            armLeft.setPosition(0.3);
         }
 
         if (gamepad1.right_bumper) {
-            armRight.setPosition(gripperOpenPosition);
+            armRight.setPosition(0.56);
         }
 
-        else {
-            armRight.setPosition(gripperClosedPosition);
+        if (gamepad1.dpad_right){
+            armRight.setPosition(0);
         }
 
         telemetry.addData("Status", "Run Time: " + runtime.toString());
         telemetry.addData("Manual Power", manualArmPower);
-
+        telemetry.addData("wrist dowm", wristDownPosition);
+        telemetry.addData("wrist up", wristUpPosition);
+        telemetry.addData("Arm Pos:",
+                        ", arm = " +
+                        ((Integer)arm.getTargetPosition()).toString());
+        telemetry.addData("Arm Pos:",
+                        ", arm = " +
+                        ((Integer)arm.getCurrentPosition()).toString());
     }
 
     /*
@@ -189,4 +157,9 @@ public class minirobot extends OpMode
     public void stop() {
     }
 
+
 }
+
+
+
+
